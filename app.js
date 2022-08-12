72b5bc431d8664ab1e0bab4d56d05251bbb53779
Felipe Pouyssegur <felipepouyssegur@hotmail.com>
2022-08-12 10:51:30 -0300

    primer commit :D

---
+++ app.js
@@ -0,0 +1,54 @@
+
+/* let nombreAlumno = "Juan Pablo"; 
+console.log(nombreAlumno)
+let edad = 32; 
+console.log(edad)
+let estudiante = true; 
+console.log(estudiante)
+
+const apellido = "Rodriguez"
+console.log(apellido)
+
+ */
+
+
+//Diferencias entre LET y CONST
+
+// pueden inicializarse vacias
+// pueden ser redeclaradas
+/* let nombre;
+nombre = "Coder"
+nombre = "House"
+nombre = 37
+console.log(nombre) */
+
+
+// deben ser declaradas con valor, NO pueden inicializarse vacias
+// NO pueden ser redeclaradas
+
+/* const NOMBRE= "Coder"; */
+
+/* let nombre = "Juan Pablo";
+let apellido = "Rodriguez";
+
+let nombreCompleto = nombre + "" + apellido;
+
+console.log(nombreCompleto); */
+
+/* let numero1 = 25;
+let numero2 = 35;
+
+let resultado = numero1 / numero2;
+
+console.log(resultado); */
+
+/* let nombre = prompt("Ingrese su nombre.")
+
+alert("Hola" + " " + nombre) */
+
+let añoNacido = prompt("¿En que año naciste?");
+const añoActual = prompt("¿En que año estamos?");
+
+let resultado = parseInt(añoActual) - parseInt(añoNacido);
+
+alert(resultado);
